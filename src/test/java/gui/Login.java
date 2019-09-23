c98b27517123445b74643f31d66a082b1ff94a2d
Boris Osyanin <osyanin@gmail.com>
2019-09-23 18:37:11 +0300

    junit assert allure test

--- src/test/java/gui/Login.java
+++ src/test/java/gui/Login.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Login extends TestBase {
     @Test
     void vkBoNegative() {
         open("https://dev.joinrpg.ru/account/login");
         $("#VKontakte").click();
+        assertEquals("1", "2");
         //$(byLinkText("Мои заявки")).isDisplayed();
     }
 
